6f8f277f823cfaa67356a9209025ad0a36ce3306
Elise Linn <(none)>
2017-07-30 22:09:15 -0700

    update .gitignore and add new author files

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 1,
+            firstName: 'j3',
+            lastName: 'jk'
+        },
+        {
+            id: 2,
+            firstName: 'au',
+            lastName: 'ab'
+        },
+        {
+            id: 3,
+            firstName: 'lol',
+            lastName: 'wtf'
+        }
+    ]
+};
