00de5a82361a8fb30af2f3f81afafd3cf12f7f32
Elise Linn <(none)>
2017-07-30 19:15:49 -0700

    add common header

---
+++ src/components/common/header.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var React, Header;
+
+React = require('react');
+
+Header = React.createClass({
+    render: function () {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <ul className="nav navbar-nav">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/#about">About</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+});
+
+module.exports = Header;
